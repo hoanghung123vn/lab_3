27f141cd764612ded4a5a58e0e189d3ebc38dc47
hunghv2 <hunghv2@sapo.vn>
2020-10-21 10:32:20 +0700

    Init project

---
+++ src/main/java/model/Todo.java
@@ -0,0 +1,31 @@
+package model;
+
+public class Todo {
+    private int id;
+    private String title;
+    private boolean status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
